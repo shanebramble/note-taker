2b1c8dddc170343c7d585a1c2436f83a467a7230
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-02 17:02:12 -0400

    added /api/db route for all notes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+// Reference the db json file.
+const {db} = require('./db/db');
 
 const app = express();
 // The following code to serve images, CSS files, and JavaScript files in a directory named public:
 app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3001;
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/db', (req, res) => {
+    res.json(db);
+});
 
 // A route to serve main index.html when loaded first.
 app.get('/', (req, res) => {
